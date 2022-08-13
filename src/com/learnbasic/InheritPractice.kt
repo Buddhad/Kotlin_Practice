99c83e10a25fd3d1eee1e4b3f9726f7ea2e83174
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2022-08-13 22:05:20 +0530

    inheritance practice

--- src/com/learnbasic/InheritPractice.kt
+++ src/com/learnbasic/InheritPractice.kt
@@ -1,2 +1,31 @@
 package com.learnbasic
+//parent class
+open class Animal{
 
+    fun eat(){
+        println("He can eat")
+    }
+    fun run(){
+        println("He can run")
+    }
+
+    fun sleep()
+    {
+        println("He can sleep")
+    }
+}
+//Child class
+class Dog:Animal(){
+    fun walk(){
+        println("He can walk")
+    }
+}
+
+fun main(){
+val obj= Dog()
+
+    obj.walk()
+    obj.run()
+    obj.eat()
+    obj.sleep()
+}
